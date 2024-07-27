0672e1670b8a32eaacbf14fa32434595de5d1120
rafaelblt <rafinha.b.lucena@gmail.com>
2024-07-27 12:13:33 -0300

    primeiro commit

---
+++ main/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.alunoView, name='aluno-lista'),
+    path('alunoID/<int:id>', views.alunoIDview, name='aluno-detalhe'),
+]
